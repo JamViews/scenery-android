ef4276277766dfd671a4871409f9c072c49f4f62
derek <derek@llamabagel.ca>
2018-09-15 01:04:44 -0400

    Added CameraView implementation

--- app/src/main/java/scenery/app/MainActivity.kt
+++ app/src/main/java/scenery/app/MainActivity.kt
@@ -2,11 +2,19 @@ package scenery.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.otaliastudios.cameraview.Gesture
+import com.otaliastudios.cameraview.GestureAction
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        camera.setLifecycleOwner(this)
+
+        // Activate tap-to-focus
+        camera.mapGesture(Gesture.TAP, GestureAction.FOCUS)
     }
 }
